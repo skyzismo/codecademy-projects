adfd2132fb2e3d2a6875dfc0cf21241eb42d4584
skyzismo <lucas.petigato@hotmail.com>
2021-03-26 18:43:17 -0400

    adcionada primeira função

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -2,5 +2,8 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
 
+console.log(generateTarget());
